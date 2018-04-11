3e04d935bc73e4270dc8e58696e5bbee09cb6db6
Katrina <zhaoyanlin2016@gmail.com>
2018-04-11 10:03:19 -0700

    Complete practice06. Create Teacher class that have two constructors.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+
+        if (klass == null) {
+            return -1;  // to avoid null pointer exception
+        }
+
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+
+        return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+    }
 }
