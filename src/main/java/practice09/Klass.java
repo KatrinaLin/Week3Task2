7e015bae699879f8cf1c2ecc4437dd4e07c0daac
Katrina <zhaoyanlin2016@gmail.com>
2018-04-11 11:37:10 -0700

    Complete practice09. Added appendMember method in Klass class.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,47 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private int number;
+    private Student leader;
+    private List<Student> classStudents;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void appendMember (Student student) {
+
+        if (classStudents == null) {
+            classStudents = new ArrayList<>();
+        }
+
+        classStudents.add(student);
+    }
+
+    public void assignLeader (Student student) {
+
+        if (classStudents == null || !classStudents.contains(student)) {
+            System.out.println("It is not one of us.");
+            return;
+        }
+
+        leader = student;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
 }
--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,29 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        String role = "";
+
+        if (klass.getLeader() != null && klass.getLeader().equals(this)) {
+            role = "Leader of ";
+        } else {
+            role = "at ";
+        }
+
+        return super.introduce() + " I am a Student. I am " + role + klass.getDisplayName() + ".";
+    }
+
 }
