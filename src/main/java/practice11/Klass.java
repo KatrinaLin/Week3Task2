929c73a46d32af381bf7c3a84f4131369c258bd4
Katrina <zhaoyanlin2016@gmail.com>
2018-04-11 21:49:48 -0700

    Working on practice11.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,61 @@
 package practice11;
 
+import java.util.HashSet;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private HashSet<Student> classStudents;
+    private HashSet<Teacher> teachers;
+
+    public Klass(int number) {
+        this.number = number;
+        classStudents = new HashSet<>();
+        teachers = new HashSet<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void appendMember (Student student) {
+        classStudents.add(student);
+
+        if (teachers != null && teachers.size() != 0) {
+            teachers.stream()
+                    .forEach(t -> System.out.println("I am " + t.getName() + ". I know " + student.getName() + "has joined " + getDisplayName()));
+        }
+    }
+
+    public void assignLeader (Student student) {
+
+        if (!isIn(student)) {
+            System.out.println("It is not one of us.");
+            return;
+        }
+
+        leader = student;
+
+        if (teachers != null) {
+            teachers.stream()
+                    .forEach(t -> System.out.println("I am " + t.getName() + ". I know " + student.getName() + "become Leader of " + getDisplayName()));
+        }
+    }
+
+    public void assignTeacher (Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public boolean isIn(Student student) {
+        return (classStudents != null) && classStudents.contains(student);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
 }
--- src/main/java/practice11/Student.java
+++ src/main/java/practice11/Student.java
@@ -1,4 +1,30 @@
 package practice11;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        klass.appendMember(this);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+
+        String role = "";
+
+        if (klass.getLeader() != null && klass.getLeader().equals(this)) {
+            role = "Leader of ";
+        } else {
+            role = "at ";
+        }
+
+        return super.introduce() + " I am a Student. I am " + role + klass.getDisplayName() + ".";
+    }
+
 }
